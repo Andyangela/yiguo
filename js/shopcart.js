82a3eb54bfbe092cb519e41708a89597dfda0525
Fonci <1780498903@qq.com>
2019-06-09 17:39:15 +0800

    购物车页面

--- js/shopcart.js
+++ js/shopcart.js
@@ -1,5 +1,5 @@
-$(function() {
-    // 判断用户是否登录
+$(function () {
+    // ======判断用户是否登录======
     var userLogin = localStorage.getItem("username");
     if (userLogin) { //已登录
         $("header").show();
@@ -7,29 +7,26 @@ $(function() {
     } else { //未登录
         $("header").hide();
         $(".cart_login").show();
-    }
-
-
-
-    //商品列表渲染
+    };
+    //======商品列表渲染======
     function list() {
         $.ajax({
             url: "../../php/shopcart.php",
             type: "get",
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 var html = "";
                 for (var i = 0; i < data.length; i++) {
                     html += `
                     <div class="list_box">
                         <div class="list_check">
-                            <i class="active checked"></i>
+                            <i class="active"></i>
                         </div>
                         <div class="img">
                             <img src="${data[i].img_url}" alt="">
                         </div>
                         <div class="text">
-                            <h2 class="goodsName">${data[i].goodsName}</h2>
+                            <h2 class="goodsName" name="goodsName">${data[i].goodsName}</h2>
                             <div class="stripe"></div>
                             <p><strong>￥</strong><b>${data[i].price}</b></p>
                         </div>
@@ -37,103 +34,160 @@ $(function() {
                             <i></i>
                         </div>
                         <div class="num">
-                            <span class="minus"><i></i></span><span class="count">${data[i].count}</span><span class="plus"><i></i></span>
+                            <span class="minus"><i></i></span><span class="count" name="count">${data[i].count}</span><span class="plus"><i></i></span>
                         </div>
                     </div>
                     `
                 }
                 $(".goods").html(html);
-                chooseGoods();
-                plusBtn();
+                delGoods();//删除商品
+                btnControl();//按钮控制
+                plus_btn();//增加按钮
+                minus_btn();//减少按钮
+                total_price(); //总商品和总价格
             }
-        })
-    }
-    list();
-
-
-    // 如果购物车内有商品，按钮全部默认 开启
-    function chooseGoods() {
-        if ($(".nogoods").hide() && $(".goods").show()) {
-            $(".total_cart .check i").removeClass("no").addClass("active");
-            $(".total_cart .btn a").removeClass("no");
-            login();
-            $(".total_cart .check i").on("click", function() { //全选-----按钮控制
-                    if ($(this).attr("class") == "active") { //取消全选按钮
-                        $(".total_cart .check i").removeClass("active");
-                        $(".list_check i").removeClass("active");
-                        $(".total_cart .btn a").addClass("no");
-                        $(".total_cart .btn a").on("click", function() { //没有选中商品时，结算按钮不能点击
+        });
+        // ======按钮控制======
+        function btnControl() {
+            if ($(".nogoods").hide() && $(".goods").show()) {  //加购后，购物车显示内容
+                $(".total_cart .check i").addClass("active"); //全选按钮默认开启
+                $(".total_cart .btn a").removeClass("no");//结算按钮开启
+                login();//点击结算按钮，判断用户是否登录
+                // -------------------全选按钮控制-------------------
+                $(".total_cart .check i").on("click", function () { //点击全选按钮
+                    $(this).toggleClass('active');
+                    if ($(this).hasClass("active")) { //选中全选，
+                        $(".total_cart .btn a").removeClass("no");//结算按钮开启
+                        $(".list_check i").addClass("active");//单个按钮全部选中
+                        total_price();//总数量和总价格
+                    } else {                          //取消全选，
+                        $(".total_cart .btn a").addClass("no");//结算按钮关闭
+                        $(".total_cart .btn a").on("click", function () { //没有选中商品时，结算按钮不能点击
                             location.href = "javascript:;";
-                        })
-                    } else { //全选按钮开启
+                        });
+                        $(".list_check i").removeClass("active");////单个按钮全部取消
+                        total_price();//总数量和总价格
+                    }
+                });
+                // ----------------------单个按钮控制-------------------
+                $(".list_check i").on("click", function () { //点击单个按钮
+                    $(this).toggleClass("active");
+                    var goods_length = $(".list_check i").length;//列表数量
+                    var checked_length = $('.list_box').find('.list_check i').filter('.active').length;//选中列表的数量
+                    if (goods_length == checked_length) {//两者相等，开启全选按钮，
                         $(".total_cart .check i").addClass("active");
-                        $(".list_check i").addClass("active");
-                        $(".total_cart .btn a").removeClass("no"); //结算按钮开启
-                        login();
+                        $(".total_cart .btn a").removeClass("no");//结算按钮开启
+                        total_price();//总数量和总价格
+                    } else {//否则 
+                        $(".total_cart .check i").removeClass("active");//关闭全选
+                        $(".total_cart .btn a").removeClass("no");//结算按钮开启
+                        total_price();//总数量和总价格
+                        if (checked_length == 0) { //如果没有选择商品
+                            $(".total_cart .btn a").addClass("no");//结算按钮关闭
+                            $(".total_cart .btn a").on("click", function () { //没有选中商品时，结算按钮不能点击
+                                location.href = "javascript:;";
+                            });
+                            total_price();//总数量和总价格
+                        }
                     }
                 })
-                // 单个-----按钮控制
-            $(".list_check i").on("click", function() {
-                if ($(this).attr("class") == "checked") { //未选中商品，
-                    $(this).attr("class", "checked active"); //点击选中要选的商品
-                    if ($(this).attr("class") == "checked active") { //选中部分商品时，全选按钮不开启，结算按钮开启
-                        $(".total_cart .btn a").removeClass("no");
-                        if ($(".list_box").length > 1) {
-                            $(".total_cart .check i").removeClass("active");
-                        } else {
-                            $(".total_cart .check i").addClass("active");
-                        }
-                        //选中全部商品时，按钮全部开启
-                        if ($(this).parents(".list_box").siblings().children().eq(0).children().attr("class") == "checked active") {
-                            $(".total_cart .btn a").removeClass("no");
-                            $(".total_cart .check i").addClass("active");
-                        }
-                    }
-                } else { //已选中全部商品
-                    $(this).attr("class", "checked"); //点击取消所选商品
-                    $(".total_cart .check i").removeClass("active"); //取消全选按钮
-                    if ($(".list_box").length = 1) {
-                        $(".total_cart .btn a").removeClass("no");
-                    }
-                    if ($(this).attr("class") == "checked") {
-                        if ($(".list_box").length = 1) {
-                            $(".total_cart .btn a").removeClass("no");
-                        }
-                        if ($(this).parents(".list_box").siblings().children().eq(0).children().attr("class") == "checked") {
-                            //取消全部商品时，所以按钮关闭
-                            $(".total_cart .btn a").addClass("no");
-                        }
-                    }
-                }
-            })
-        }
-    }
-
-
+            }
+        };
+    };
+    list();
+    //======结算时判断是否登录======
     function login() {
-        $(".total_cart .btn a").on("click", function() { //选购商品，进行结算时，要先判断用户是否登录
+        $(".total_cart .btn a").on("click", function () { //选购商品，进行结算时，要先判断用户是否登录
             if (userLogin) {
                 location.href = ""; //已登录跳转到下一个页面
             } else {
                 location.href = "../../html/login/login.html"; //未登录，跳转到登录页面
             }
         })
-    }
-
-    //商品加减按钮
-    function plusBtn() {
-        var index = 1;
-        $(".plus").on("click", function() {
-            index++;
-            $(".count").html(index)
+    };
+    //======增加按钮======
+    function plus_btn() {
+        $(".plus").on("click", function () {
+            var count = $(this).parents(".list_box").find(".count").html();//加购数量
+            var goodsName=$(this).parents(".list_box").find(".goodsName").html(); //当前商品名称
+            count++;
+            $.ajax({
+                url: "../../php/plus.php",
+                data: {
+                    count: count,
+                    goodsName:goodsName
+                },
+                type: "get",
+                dataType: "json",
+                success: function (data) {}
+            });
+            $(this).parents(".list_box").find(".count").html(count);//显示该商品的数量
+            total_price(); //总商品和总价格
         })
-        $(".minus").on("click", function() {
-            index--;
-            if (index <= 1) {
-                index = 1;
+    };
+    //======减少按钮=======
+    function minus_btn(){
+        $(".minus").on("click",function(){
+            var count = $(this).parents(".list_box").find(".count").html();//加购数量
+            var goodsName=$(this).parents(".list_box").find(".goodsName").html(); //当前商品名称
+            count--;
+            if(count<=1){
+                count=1;
             }
-            $(".count").html(index)
+                $.ajax({
+                    url: "../../php/plus.php",
+                    data: {
+                        count: count,
+                        goodsName:goodsName
+                    },
+                    type: "get",
+                    dataType: "json",
+                    success: function (data) {}
+                });
+                $(this).parents(".list_box").find(".count").html(count);//显示该商品的数量
+                total_price(); //总商品和总价格
         })
-    }
+    };
+    //======商品删除按钮======
+    function delGoods() {
+        $(".del i").on("click", function () { //点击删除按
+            $(".shade").show();  //弹出是否删除 确认框
+            $(".btnbox .yes").on("click", function () {  //点击确定按钮，发起ajax请求
+                $.ajax({
+                    url: "../../php/del.php",
+                    data: {
+                        goodsName: $(".goodsName").html()
+                    },
+                    dataType: "json",
+                    type: "GET",
+                    success: function () {
+                        location.assign(location) //删除后自动刷新页面
+                    }
+                });
+                $(".shade").hide();
+            });
+            $(".btnbox .no").on("click", function () { //点击取消按钮,确认框消失
+                $(".shade").hide();
+            })
+        })
+    };
+
+    // ======总数量、总价格======
+    function total_price() {
+        var allPrice = 0;//总价格
+        var allNum = 0;//总数量
+        $(".list_check i").each(function () {
+            if ($(this).hasClass("active")) {
+                var num = parseInt($(this).parents('.list_box').find('.count').html());//单个商品的购买数量
+                var price = parseFloat($(this).parents('.list_box').find('.text b').text());//商品单价
+                allPrice += num * price //每个商品的总价
+                allNum += num;  //总数量
+            }
+        })
+        $(".allPrice").html(allPrice.toFixed(2)) //总价格
+        $(".total_num").html(allNum) //总数量
+    };
+
+
 
 })
